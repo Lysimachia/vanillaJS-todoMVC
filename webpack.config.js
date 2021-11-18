2f566add2362d96293fd7ca00a462c459b50c6dc
Lysimachia <91607971+Lysimachia@users.noreply.github.com>
2021-11-18 22:33:08 +0900

    set webpack env

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        open: true,
+        host: "localhost",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html",
+        }),
+        new MiniCssExtractPlugin({ filename: 'style.css' })
+    ],
+    module: {
+        rules: [{
+                test: /\.(js|jsx)$/i,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: "asset",
+            },
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = "production";
+
+        config.plugins.push(new MiniCssExtractPlugin());
+    } else {
+        config.mode = "development";
+    }
+    return config;
+};
